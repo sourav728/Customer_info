deadb49b91d4eb6ae6e3a5647cd159b4e739ed7d
Sourav Nandi <souravnandi728@outlook.com>
2018-04-18 18:07:17 +0530

    multiple language support option layout integrated

--- app/src/main/java/com/example/tvd/customer_info/fragments/More_fragment.java
+++ app/src/main/java/com/example/tvd/customer_info/fragments/More_fragment.java
@@ -1,20 +1,25 @@
 package com.example.tvd.customer_info.fragments;
 
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.example.tvd.customer_info.R;
 
 public class More_fragment extends Fragment {
-
-
+    RelativeLayout language;
+    AlertDialog alertDialog1;
+    CharSequence[] values = {"English","Kanada"};
     public More_fragment() {
     }
 
@@ -28,6 +33,13 @@ public class More_fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_more_fragment, container, false);
+        language = (RelativeLayout) view.findViewById(R.id.relative_language);
+        language.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreateAlertDialogWithRadioButtonGroup() ;
+            }
+        });
         return view;
     }
 
@@ -35,5 +47,32 @@ public class More_fragment extends Fragment {
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.menu_more,menu);
         super.onCreateOptionsMenu(menu, inflater);
+    }
+        public void CreateAlertDialogWithRadioButtonGroup(){
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setTitle("Select Your Choice");
+
+        builder.setSingleChoiceItems(values, -1, new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int item) {
+
+                switch(item)
+                {
+                    case 0:
+                        Toast.makeText(getActivity(), "First Item Clicked", Toast.LENGTH_LONG).show();
+                        break;
+                    case 1:
+
+                        Toast.makeText(getActivity(), "Second Item Clicked", Toast.LENGTH_LONG).show();
+                        break;
+                }
+                alertDialog1.dismiss();
+            }
+        });
+        alertDialog1 = builder.create();
+        alertDialog1.show();
     }
 }
