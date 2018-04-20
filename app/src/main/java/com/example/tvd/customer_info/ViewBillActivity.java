76b6843b0e3b913ff049a31e1bc92f95c18e9052
Sourav Nandi <souravnandi728@outlook.com>
2018-04-20 13:07:44 +0530

    View Bill part added

--- app/src/main/java/com/example/tvd/customer_info/ViewBillActivity.java
+++ app/src/main/java/com/example/tvd/customer_info/ViewBillActivity.java
@@ -1,13 +1,42 @@
 package com.example.tvd.customer_info;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
 
 public class ViewBillActivity extends AppCompatActivity {
-
+    private Toolbar toolbar;
+    Typeface typeface;
+    TextView font_toolbar_title,font_billing_details,font_billing_period,font_reading_date,font_bill_no,font_due_date,
+             font_personal_details,font_account_details,font_connection_details;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_bill);
+        initialize();
+        toolbar.setNavigationIcon(R.drawable.back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+    public void initialize()
+    {
+        typeface = Typeface.createFromAsset(getAssets(),"timesnewroman.ttf");
+        toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        font_toolbar_title = (TextView) toolbar.findViewById(R.id.toolbar_title);
+        font_billing_details = (TextView) findViewById(R.id.txt_billing_details);
+        font_billing_period = (TextView) findViewById(R.id.txt_billing_period);
+        font_reading_date = (TextView) findViewById(R.id.txt_reading_date);
+        font_bill_no = (TextView) findViewById(R.id.txt_bill_no);
+        font_due_date = (TextView) findViewById(R.id.txt_due_date);
+        font_personal_details = (TextView) findViewById(R.id.txt_personal_details);
+        font_account_details = (TextView) findViewById(R.id.txt_account_details);
+        font_connection_details = (TextView) findViewById(R.id.txt_connection_details);
     }
 }
--- app/src/main/java/com/example/tvd/customer_info/fragments/home_fragment.java
+++ app/src/main/java/com/example/tvd/customer_info/fragments/home_fragment.java
@@ -1,13 +1,16 @@
 package com.example.tvd.customer_info.fragments;
 
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.tvd.customer_info.R;
 import com.example.tvd.customer_info.adapter.ViewPagerAdapter;
@@ -19,21 +22,23 @@ public class home_fragment extends Fragment {
     Home_fragment2 homeFragment;
     Account_fragment account_fragment;
     More_fragment more_fragment;
+
     public home_fragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view =  inflater.inflate(R.layout.fragment_home_fragment, container, false);
+        View view = inflater.inflate(R.layout.fragment_home_fragment, container, false);
         viewPager = (ViewPager) view.findViewById(R.id.viewpager);
         viewPager.setOffscreenPageLimit(3);
         setupViewPager(viewPager);
         tabLayout = (TabLayout) view.findViewById(R.id.tab_layout);
+
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition(),false);
+                viewPager.setCurrentItem(tab.getPosition(), false);
             }
 
             @Override
@@ -64,15 +69,15 @@ public class home_fragment extends Fragment {
         });
         return view;
     }
-    private void setupViewPager(ViewPager viewPager)
-    {
+
+    private void setupViewPager(ViewPager viewPager) {
         adapter = new ViewPagerAdapter(getActivity().getSupportFragmentManager());
         homeFragment = new Home_fragment2();
         account_fragment = new Account_fragment();
         more_fragment = new More_fragment();
-        adapter.addFragment(homeFragment,"Home");
+        adapter.addFragment(homeFragment, "Home");
         adapter.addFragment(account_fragment, "Account");
-        adapter.addFragment(more_fragment,"More");
+        adapter.addFragment(more_fragment, "More");
         viewPager.setAdapter(adapter);
     }
 
